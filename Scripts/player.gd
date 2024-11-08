2999b5daac53d749377b47a726f5b8e4b0fb8541
eiganesteknolab <nav@eiganesteknolab.com>
2024-11-08 18:49:03 +0100

    Controller support

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,37 +3,47 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+const SHOOT_COOLDOWN = 0.2
+const JUMP_COOLDOWN = 0.2
+
+var can_shoot = true
+var can_jump = true
+
 # Exported variable for the bullet scene
 @export var Bullet: PackedScene
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	if can_jump and  Input.is_joy_button_pressed(0, JOY_BUTTON_X):
 		velocity.y = JUMP_VELOCITY
+		can_jump = false
+		await get_tree().create_timer(JUMP_COOLDOWN).timeout
+		can_jump = true
+		
+	# Check for shooting input
+	if can_shoot and Input.is_joy_button_pressed(0, JOY_BUTTON_A):
+		shoot()
+		can_shoot = false
+		await get_tree().create_timer(SHOOT_COOLDOWN).timeout
+		can_shoot = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-			
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	move_and_slide()
 
 	if velocity.x > 0:
 		$AnimatedSprite2D.flip_h = false
 	elif velocity.x < 0:
 		$AnimatedSprite2D.flip_h = true
 		
-		# Check for shooting input
-	if Input.is_action_just_pressed("ui_up"):
-		shoot()
+	move_and_slide()
 		
 func shoot() -> void:
 	var b = Bullet.instantiate()
