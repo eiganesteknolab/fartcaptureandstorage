e84b3a38b435f88bdf5ba047e2824f06f9b419fd
eiganesteknolab <nav@eiganesteknolab.com>
2024-11-08 16:45:17 +0100

    Diper pooping Cows

--- Scripts/cow.gd
+++ Scripts/cow.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
 @export var Fart: PackedScene
+@export var FullDyper: PackedScene
+
+
+func _ready():
+	$fullTimer.paused = true
 
 func _on_area_entered(_area: Area2D) -> void:
 	print("Cow got hit by something")
@@ -8,8 +13,10 @@ func _on_area_entered(_area: Area2D) -> void:
 	#print(_area.get_path())
 	#print(_area.get_groups())
 	if _area.is_in_group("emptyDiper"):
-		$EmptyDiper.visible = true
-		$ch4Timer.paused = true
+		if $EmptyDiper.visible == false:
+			$EmptyDiper.visible = true
+			$ch4Timer.paused = true
+			$fullTimer.paused = false
 			
 		print("Cow got hit by diper")
 		_area.queue_free()
@@ -18,8 +25,18 @@ func _on_area_entered(_area: Area2D) -> void:
 func _on_ch_4_timer_timeout() -> void:
 	fart()
 
-
 func fart() -> void:
 	var b = Fart.instantiate()
 	owner.add_child(b)
 	b.global_transform = $FartPoint.global_transform
+
+func _on_full_timer_timeout() -> void:
+	fullDiper()
+	$EmptyDiper.visible = false
+	$ch4Timer.paused = false
+	$fullTimer.paused = true
+	
+func fullDiper() -> void:
+	var c = FullDyper.instantiate()
+	owner.add_child(c)
+	c.global_transform = $FartPoint.global_transform
